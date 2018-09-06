d99efba39c934117ebc22ef67a45921021ba5b17
matianfu <matianfu@gmail.com>
2018-09-06 14:51:35 +0000

    tmp

---
+++ app.js
@@ -0,0 +1,23 @@
+const Promise = require('bluebird')
+const i2c = require('i2c-bus')
+const Ecc = require('./lib/ecc') 
+
+const i2c1 = i2c.open(1, err => {
+  if (err) {
+    console.log('failed to open i2c1')
+  } else {
+    let ecc = new Ecc(i2c1) 
+    ecc.on('initialized', () => {
+      console.log('ecc initialized', '0x' + ecc.addr.toString(16).toUpperCase())
+
+      ;(async () => {
+        let config = await ecc.atcabReadConfigZoneAsync()
+        console.log(config)
+      })().then(x => x).catch(e => console.log(e))
+
+    })
+  }
+})
+
+
+
