27fa21838393d277a275d2fe18f492a14303924e
matianfu <matianfu@gmail.com>
2018-09-09 16:26:45 +0000

    tmp

--- app.js
+++ app.js
@@ -13,18 +13,48 @@ const i2c1 = i2c.open(1, err => {
       ;(async () => {
         let config = await ecc.atcabReadConfigZoneAsync()
   
-        console.log(config)
-       
-        await ecc.writeAWSConfigAsync() 
+//        console.log(config)
+//        console.log(config[86], config[87]) 
+/**
+        config = await ecc.writeAWSConfigAsync() 
 
-        config = await ecc.atcabReadConfigZoneAsync()
         console.log(config)
+
+        await ecc.atcabLockConfigZoneAsync ()
+
+        console.log('config zone locked')
+
+        await ecc.atcabLockDataZoneAsync ()
+
+        console.log('data zone locked')
+
+        await ecc.wakeAsync()
+        await ecc.sleepAsync()
+
+        console.log('done')
+*/
+
+        console.log(ecc.addr.toString(16))
+
+        let pubkey
+
+        pubkey = await ecc.atcabGenKeyAsync(0)
+        console.log('pubkey', pubkey)
+
+        pubkey = await ecc.atcabGenPubKeyAsync(0)
+        console.log('pubkey', pubkey)
+
+//        pubkey = await ecc.atcabGenPub
+
+//        await ecc.atcabGenKeyAsync(2)
+//        await ecc.atcabGenKeyAsync(3)
+//        await ecc.atcabGenKeyAsync(7)
+
 
       })().then(x => x).catch(e => console.log(e))
 
     })
   }
 })
-
 
 
